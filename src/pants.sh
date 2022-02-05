2df13640f1b1f67ed26081774280ebfe715ec0e2
Andreas Stenius <andreas.stenius@svenskaspel.se>
2022-02-05 15:42:27 +0100

    Expand path args so they are always relative to repo root.

--- src/pants.sh
+++ src/pants.sh
@@ -1,22 +1,60 @@
 #!/usr/bin/env sh
 # This is a thin Pants Build System shim, to locate and invoke the closest parent `pants` bootstrap
 # script from the CWD.
+set -euf -o pipefail
 
+export PANTS_BIN_NAME=${PANTS_BIN_NAME:-pants}
+relpath=
+pants=
 
 function find_pants_script {
     local path="$1"
-    while [[ ! -x "${path}/pants" || ! -f "${path}/pants" ]]; do
+    local script="${path}/${PANTS_BIN_NAME}"
+
+    while [[ ! -x "${script}" || ! -f "${script}" ]]; do
         if [[ "${path}" = "/" ]]; then
-            echo "Error: no \`pants\` script found in or above of the directory ${1}" >&2
+            echo "Error: \`${PANTS_BIN_NAME}\` not found in or above of the directory ${1}" >&2
             exit 127
         fi
         path=$(dirname "${path}")
+        script="${path}/${PANTS_BIN_NAME}"
     done
-    echo "${path}/pants"
+
+    pants="${script}"
+    if [[ "${path}" != "${1}" ]]; then
+        # Strip off the path to the repo root, so we can append that to all file path args.
+        relpath="${1#${path}/}/"
+    fi
 }
 
+function expand_path_parameters {
+    for arg; do
+        local opt=
+        local val="${arg#*=}"
 
-pants="$(find_pants_script $(pwd))"
+        if [[ "${arg}" =~ "=" ]]; then
+            opt="${arg%%=*}="
+        fi
+
+        local maybe_path="${val%%:*}"
+        local target=
+
+        if  [[ "${val}" =~ ":" ]]; then
+            target="${val#*:}"
+        fi
+
+        if [[ -e "${maybe_path}" ]]; then
+            # Stitch everything together after adding the relpath from the repo root to our CWD.
+            echo "${opt}${relpath}${maybe_path}${target}"
+        else
+            # Not a file path argument, leave as-is.
+            echo "${arg}"
+        fi
+    done
+    set +x
+}
+
+find_pants_script "${PWD}"
 if [[ -n "${pants}" ]]; then
-    exec "${pants}" "$@"
+    exec "${pants}" $(expand_path_parameters "$@")
 fi
